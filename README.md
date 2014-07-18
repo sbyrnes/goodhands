3932e3f6dd8c9e7f8f32b59942cd2bb92e835d1a
sean <sean.byrnes@gmail.com>
2014-07-17 22:00:19 -0700

    First commit, prototyping in progress

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-goodhands
+GoodHands
 =========
 
-A prototype glove for controlling mobile applications via bluetooth. 
+A prototype glove for controlling mobile applications via finger movements. Currently in prototyping stages.
+
+Project Goals
+-----
+The goal of the GoodHands project is to create a glove which, using multiple sensors, allows the user to communicate with their phone using various finger positions and movements.
+
+There are various applications of this kind of wearable:
+ 1. Allow for robust phone control without looking at a screen (phone or watch).
+ 2. Enable your phone to act as a speech generator for sign language.
+ 3. Observation and recording of finger movements when performing a task.
+
+Of these, the sign language application is the major driving factor and part of the GoodHands project will include a simple Android Application to teach the American Sign Language alphabet.
+
+Note that while some of these applications can be achieve with remote motion sensors (such as Kinect) or through gesture detectors, this is designed to be used in an arbitrary location and be sensitive to very detailed finger configurations.
+
+Technical Components
+-----
+While the design of the prototype is still underway, it will be based on an Arduino Uno microcontroller.
+
+You can find both the Arduino software and the circuit schematics in the respective subdirectories of this project.
+To view the circuit diagrams, you will need to download Fritzing (http://fritzing.org/home/), a free schematics tool for Arduino projects.
+
+Challenges
+-----
+Challenges are significant for any hardware project. In this particular case, major challenges that need to be investigated and overcome include:
+ * Finding a sensor type that is small enough and cheap enough to create a sensor matrix throughout a glove. Initial investigation has centered on capacitive sensors which are promising but may not work in real world applications.
+ * Enabling Bluetooth Low Energy (BLE) communication between the glove and the phone at a reasonable price point. There is a lack of well supported BLE shields for Arduino (at least as identified so far).
+ * Ensuring that the glove, when done, does not meaningfully impede normal behavior and/or feeling. 
---
+++ code/goodhands/goodhands.ino
@@ -0,0 +1,88 @@
+#include <CapacitiveSensor.h>
+
+/*
+ * GoodHands Main Control Program v0.1
+ * https://github.com/sbyrnes/goodhands
+ * Author: Sean Byrnes, Fogstack Software
+ * 
+ * Based on CapitiveSense Library Demo Sketch by Paul Badger 
+ * (http://playground.arduino.cc/Main/CapacitiveSensor?from=Main.CapSense)
+ *
+ * The CapacitiveSensor Library works by creating changes in the voltage between two pins and measuring the
+ * delay between when the voltage is changed on the send pin and received on the sensor pin. With no capacitance
+ * the delay is minimal, but adding capacitance (such as your finger) will dampen the oscillation and delay the change. 
+ *
+ * In GoodHands v0.1 we use three sensor pins, one for each finger, and one send pin shared by all four. 
+ * 
+ * For debugging, each sensor pin has an output pin that is activated if that sensor is active so that we can
+ * show LEDs for visual feedback.
+ */
+
+/** SET UP THE CAPACTIVE LIBRARY ENTITIES **/
+// Pin 2 is the send pin for all signals
+CapacitiveSensor   pinkySensor   = CapacitiveSensor(2,4);     // Sensing between pins 2 & 4, pin 4 is sensor pin
+CapacitiveSensor   ringSensor    = CapacitiveSensor(2,6);     // Sensing between pins 2 & 6, pin 6 is sensor pin
+CapacitiveSensor   middleSensor  = CapacitiveSensor(2,8);     // Sensing between pins 2 & 8, pin 8 is sensor pin
+CapacitiveSensor   pointerSensor = CapacitiveSensor(2,10);  // Sensing between pins 2 & 10, pin 10 is sensor pin
+
+// Output pins
+int pinkyOutput   = 5; 
+int ringOuptut    = 7; 
+int middleOutput  = 9; 
+int pointerOutput = 11; 
+
+/** Arduino Setup **/
+void setup()                    
+{
+  // Configure output pins
+  pinMode(pinkyOutput, OUTPUT);
+  pinMode(ringOutput, OUTPUT);
+  pinMode(middleOutput, OUTPUT);
+  pinMode(pointerOutput, OUTPUT);
+  
+  // configure sensors by disabling autocalibrate, probably should turn this on at some point
+   pinkySensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     
+   ringSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);   
+   middleSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);     
+   pointerSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);       
+   
+  // Enable serial communication for debugging 
+  Serial.begin(9600);
+}
+
+long kMinimumThreshold = 500;
+
+/** Main Arduino Loop **/
+void loop()                    
+{
+    long pinkyValue   =  pinkySensor.capacitiveSensor(30);
+    long ringValue    =  ringSensor.capacitiveSensor(30);
+    long middleValue  =  middleSensor.capacitiveSensor(30);
+    long pointerValue =  pointerSensor.capacitiveSensor(30);
+    
+    Serial.print(pinkyValue);                
+    Serial.print("\t");
+    Serial.print(ringValue);                 
+    Serial.print("\t");
+    Serial.print(middleValue);              
+    Serial.print("\t");
+    Serial.print(pointerValue);              
+    Serial.println("");
+
+    detectTouch(pinkyValue, pinkyOutput);
+    detectTouch(ringValue, ringOutput);
+    detectTouch(middleValue, middleOutput);
+    detectTouch(pointerValue, pointerOutput);
+
+    delay(100);  // Sensor loop delay
+}
+
+void detectTouch(long sensorValue, int outputPin){
+    if(sensorValue > kMinimumThreshold)
+    {
+      digitalWrite(outputPin, HIGH);
+    } else {
+      digitalWrite(outputPin, LOW);
+    }
+}
+
